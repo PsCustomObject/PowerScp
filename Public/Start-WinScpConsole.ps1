57e279ac9a64cbde01e1304d5a56d868ddfaa688
PsCustomObject <12457285+PsCustomObject@users.noreply.github.com>
2019-11-23 23:55:00 +0100

    Added ew function Start-WinScpConsole

---
+++ Public/Start-WinScpConsole.ps1
@@ -0,0 +1,31 @@
+﻿function Start-WinScpConsole
+{
+<#
+	.SYNOPSIS
+		Will open WinSCP Console
+	
+	.DESCRIPTION
+		Function will invoke WinSCP console and will wait till the window is closed before terminating execution.
+	
+	.EXAMPLE
+		PS C:\> Start-WinScpConsole
+#>
+	
+	[OutputType([void])]
+	param ()
+	
+	# Define WinSCP exe path
+	[string]$exePath = "$PSScriptRoot\..\bin\WinSCP.exe"
+	
+	# Define exe arguments
+	[string]$scpArgs = '/Console'
+	
+	# Launch WinSCP console
+	$paramStartProcess = @{
+		FilePath	 = $exePath
+		ArgumentList = $scpArgs
+		Wait		 = $true
+	}
+	
+	Start-Process @paramStartProcess
+}
